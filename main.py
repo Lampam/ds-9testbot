badca5cdafba55c4043bddc8059def19dc4f4399
Lampam <eqdkplus@yandex.ru>
2022-05-21 17:27:02 +0300

    try

--- main.py
+++ main.py
@@ -67,18 +67,22 @@ def test(message):
 
 		bot.send_photo(message.chat.id, 'https://disk.yandex.ru/i/gXH9SlA1I_Q7tA')
 
-@server_route('/' + TOKEN, methods = ['POST'])
-def get_message():
-	json_string = request.get_data().decode('utf-8')
-	update = telebot.types.Update.de_json(json_string)
 
-@server_route('/')
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
 def webhook():
-	bot.remote_webhook()
-	bot.set_webhook(url=APP_URL)
-	return '!',200
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
 
 if __name__ == '__main__':
-	server.run(host='0.0.0.0', port = int(os.environ.get('PORT', 5000)))
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 
