a5e5e5a9f96209f5d23c5b6f356c1986f9183b70
Lampam <eqdkplus@yandex.ru>
2022-05-21 17:47:25 +0300

    try

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ def webhook():
     return '!', 200
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+	from waitress import serve
+	serve(server, host="0.0.0.0", port=8080)
 
 
