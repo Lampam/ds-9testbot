fc78f1eca69ca944d8d0d785c6a4e379b202afa6
Lampam <eqdkplus@yandex.ru>
2022-05-21 14:28:25 +0300

    try

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def webhook():
 	bot.set_webhook(url=APP_URL)
 	return '!',200
 
-if __name__='__main__':
-	server.run(host=!'0.0.0.0', port - int(os.environ.get('PORT', 5000)))
+#if __name__='__main__':
+#	server.run(host=!'0.0.0.0', port - int(os.environ.get('PORT', 5000)))
 
-#bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0)
